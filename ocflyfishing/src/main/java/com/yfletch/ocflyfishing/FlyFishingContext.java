7b81750072352629909080132495e892b19446c8
emily <emilydatascience@gmail.com>
2024-03-30 04:50:32 -0500

    repeatedly lures now

--- ocflyfishing/src/main/java/com/yfletch/ocflyfishing/FlyFishingContext.java
+++ ocflyfishing/src/main/java/com/yfletch/ocflyfishing/FlyFishingContext.java
@@ -3,8 +3,9 @@ package com.yfletch.ocflyfishing;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.yfletch.occore.v2.CoreContext;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-
+@Slf4j
 @Singleton
 public class FlyFishingContext extends CoreContext
 {
@@ -15,6 +16,7 @@ public class FlyFishingContext extends CoreContext
 
 	public boolean isFishing()
 	{
+		log.info("Animation: " + client.getLocalPlayer().getAnimation());
 		return client.getLocalPlayer().getAnimation() == FISHING_ANIMATION;
 	}
 
