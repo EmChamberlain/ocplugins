bd7108a99b0f08487974eaf431e19077c9e2d893
emily <emilydatascience@gmail.com>
2024-03-29 19:27:45 -0500

    Added repeat to use items in bank skills

--- ocbankskills/ocbankskills.gradle.kts
+++ ocbankskills/ocbankskills.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "2.0.4"
+version = "2.0.5"
 
 project.extra["PluginName"] = "OC Bank Skills"
 project.extra["PluginDescription"] = "One-click bank skills (Herblore, Fletching, Crafting)"
