db18aa88cfba99089bac6ed77a441f4c7a156db6
emily <emilydatascience@gmail.com>
2024-03-31 01:48:26 -0500

    Made oc spells more reliable

--- ocspells/src/main/java/com/yfletch/ocspells/OCSpellsPlugin.java
+++ ocspells/src/main/java/com/yfletch/ocspells/OCSpellsPlugin.java
@@ -95,18 +95,19 @@ public class OCSpellsPlugin extends RunnerPlugin<SpellsContext>
 
 		action().name("Close bank")
 			.when(c -> Bank.isOpen() && Inventory.contains(items))
-			.then(c -> widget(WidgetID.BANK_GROUP_ID, "Close").interact());
+			.then(c -> widget(WidgetID.BANK_GROUP_ID, "Close").interact())
+			.delay(1,2);
 
 		action().name("Cast spell on item")
 			.when(c -> config.castOnItem() && !c.flag("casting") && Inventory.contains(items))
 			.then(c -> spell(spell).castOn(item(items)))
-			.delay(1,2)
+			//.delay(1,2)
 			.onClick(c -> c.flag("casting", true, 5));
 
 		action().name("Cast spell")
 			.when(c -> !config.castOnItem() && !c.flag("casting") && Inventory.contains(items))
 			.then(c -> spell(spell).cast())
-			.delay(1,2)
+			//.delay(1,2)
 			.onClick(c -> c.flag("casting", true, 5));
 
 		action().name("Casting spell")
