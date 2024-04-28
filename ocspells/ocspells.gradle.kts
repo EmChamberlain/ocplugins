b6af2d76968061b50c34c1d05c52ae45ccb8aebc
emily <emilydatascience@gmail.com>
2024-04-28 18:37:35 -0500

    Improved spells

--- ocspells/ocspells.gradle.kts
+++ ocspells/ocspells.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.2.4"
+version = "1.2.5"
 
 project.extra["PluginName"] = "OC Spells"
 project.extra["PluginDescription"] = "One-click bankstanding spells"
